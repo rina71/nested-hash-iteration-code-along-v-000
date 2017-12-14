a66e033c3a79b3f8b852f482d5d50296aec5a6e9
Yurina Shin <yurinas@gmail.com>
2017-12-14 23:55:18 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,7 @@ require 'pry'
   }
 
 def remove_strawberry(contacts)
-contacts.each {|person, contact_details_hash|
+contacts.each {|person, contact_details_hash|}
   contact_details_hash.each{|attribute,data|
     binding.pry}
 end
